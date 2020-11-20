b4a76932729f44282fd5d2d1abf3a81662b6b210
CTCheeseman <tim.mcfarland.jr@gmail.com>
2020-11-19 21:52:36 -0500

    Got insert working with a response - this will be the foundation for other POST requests

--- index.js
+++ index.js
@@ -9,25 +9,39 @@ var mysql = require('./dbcon.js');
 // requiring Cross Origin Resource Sharing
 var cors = require('cors');
 
-
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 // This is for security so the port is not shown
 const port = process.env.PORT;
 
-// BUILD QUERY STRINGS
+// *****************************
+// ********** Queries **********
+// *****************************
 
-// GET Queries
-// 
+// ***********************
+// ****** Customers ******
+// ***********************
 // Showing all customers
 const queryAllCustomers = 'SELECT * FROM customers' ;
+const insertCustomer = 'INSERT INTO customers(`first_name`, `last_name`, `email`) VALUES (?, ?, ?)' ;
 
+// **********************
+// ****** Products ******
+// **********************
 // Showing all products
 const queryAllProducts = 'SELECT * FROM products' ;
 
+// ***********************
+// ****** Addresses ******
+// ***********************
 // Showing all Addresses
 const queryAllAddresses = 'SELECT * FROM addresses' ;
+
+// *******************************
+// ****** AGGREGATE QUERIES ******
+// *******************************
 
 // Count of all customers
 const customerCount = 'SELECT COUNT(customers.customer_id) AS custCount FROM customers' ;
@@ -38,6 +52,11 @@ const completedOrders = "SELECT COUNT(order_status) AS completeOrders FROM order
 // Sum of all available products
 const availableProducts = 'SELECT SUM(products.quantity) AS availableProducts FROM products'
 
+// **********************************
+// ********** GET REQUESTS **********
+// **********************************
+
+// ****** CUSTOMERS ******
 // Get Request to return all customers
 app.get('/allCustomers', (req, res, next) => {
   var context = {};
@@ -54,7 +73,22 @@ app.get('/allCustomers', (req, res, next) => {
   });
 });
 
-// Get Request to return all products
+// AGGREGATE Request to return total customer count
+app.get('/customerCount', (req, res, next) => {
+  var context = {};
+  mysql.pool.query(customerCount, (error, results, fields) => {
+    if(error){
+      next(error); 
+      return;
+    }
+    context.results = results; 
+    console.log(context.results) 
+    res.send(context); 
+  });
+});
+
+// ****** PRODUCTS ******
+// Return all products
 app.get('/allProducts', (req, res, next) => {
   var context = {};
   mysql.pool.query(queryAllProducts, (error, results, fields) => {
@@ -68,6 +102,21 @@ app.get('/allProducts', (req, res, next) => {
   });
 });
 
+// AGGREGATE Request to return total available products
+app.get('/availableProducts', (req, res, next) => {
+  var context = {};
+  mysql.pool.query(availableProducts, (error, results, fields) => {
+    if(error){
+      next(error); 
+      return;
+    }
+    context.results = results; 
+    console.log(context.results) 
+    res.send(context); 
+  });
+});
+
+// ****** ADDRESSES ******
 // Get Request to return all addresses
 app.get('/allAddresses', (req, res, next) => {
   var context = {};
@@ -82,57 +131,51 @@ app.get('/allAddresses', (req, res, next) => {
   });
 });
 
-
-// **************** INFO BOXES AND DASHBOARD***************** //
-
-// Get Request to return total customer count
-app.get('/customerCount', (req, res, next) => {
-  var context = {};
-  // makes the query -- stores results here using the pool we created in DBCon
-  mysql.pool.query(customerCount, (error, results, fields) => {
-    // check for any returned error
-    if(error){
-      next(error); // parse all errors if multiple
-      return;
-    }
-    context.results = results; // add/update context results
-    console.log(context.results) // prints to console for server.
-    res.send(context); // send context object
-  });
-});
-
+// ****** ORDERS ******
 // Get Request to return total filled orders
 app.get('/completedOrders', (req, res, next) => {
   var context = {};
-  // makes the query -- stores results here using the pool we created in DBCon
   mysql.pool.query(completedOrders, (error, results, fields) => {
-    // check for any returned error
     if(error){
-      next(error); // parse all errors if multiple
+      next(error); 
       return;
     }
-    context.results = results; // add/update context results
-    console.log(context.results) // prints to console for server.
-    res.send(context); // send context object
+    context.results = results; 
+    console.log(context.results) 
+    res.send(context); 
   });
 });
 
-// Get Request to return total available products
-app.get('/availableProducts', (req, res, next) => {
+// ***********************************
+// ********** POST REQUESTS **********
+// ***********************************
+
+// ***********************
+// ****** CUSTOMERS ******
+// ***********************
+
+// const insertCustomer = 'INSERT INTO customers(`first_name`, `last_name`, `email`) VALUES (?, ?, ?)' ;
+
+// ****** INSERT ******
+app.post('/insertCustomer' , (req,res,next) => {
   var context = {};
-  // makes the query -- stores results here using the pool we created in DBCon
-  mysql.pool.query(availableProducts, (error, results, fields) => {
-    // check for any returned error
-    if(error){
-      next(error); // parse all errors if multiple
-      return;
+  // Object destructuring -- stores following properties from that object and
+  // storing them into variables with the following names
+  var first_name = req.body['first_name']; 
+  var last_name = req.body['last_name'];
+  var {first_name, last_name, email} = req.body;
+  mysql.pool.query(
+      insertCustomer,
+      [first_name, last_name, email],
+      // call back occurs once query is completed
+      (err, result) => {
+        if(err){
+          next(err);
+          return;
     }
-    context.results = results; // add/update context results
-    console.log(context.results) // prints to console for server.
-    res.send(context); // send context object
+    res.send(`You have added ${first_name} ${last_name}`);
   });
 });
-
 
 
 app.listen(port, () => {
